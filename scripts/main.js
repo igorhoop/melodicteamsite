cf00549d894b5fbd64544de4003eca094f27e2c2
hoop <igorhoop@yandex.ru>
2023-03-22 20:01:27 +0300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -531,3 +531,28 @@ function myFunc()
     }
 
   }
+
+
+
+  function loadXML()
+  {
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', 'books.xml', true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function () {if(this.readyState==4 && this.status==200) getData(this);}
+  }
+
+
+  function getData(xml)
+  {
+    const xmlDoc = xml.responseXML;
+    const tags = xmlDoc.getELementsByTagName('book');
+    let list = ''
+    let i = 0
+    for(i = 0; i < tags.length; i++)
+    {
+      list += '<li>' + tags[i].getELementsByTagName('title')[0].firstChild.data
+    }
+
+    document.getElementById('listbooks').innerHTML = list;
+  }
